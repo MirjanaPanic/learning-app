6400612153c2d4a2d07c2af7f86acb07a3e15b33
MirjanaPanic <panicmirjanaa@gmail.com>
2025-04-05 12:24:09 +0200

    component

---
+++ src/components/gallery.jsx
@@ -0,0 +1,19 @@
+//CHILD component - rendering inside its parent component
+function Profile() {
+  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
+}
+
+//PARENT component
+export default function Gallery() {
+  return (
+    <section>
+      <h1> Pictures: </h1>
+      <Profile />
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
+
+//Samo jedan export dozvoljen unutar jednog modula.
