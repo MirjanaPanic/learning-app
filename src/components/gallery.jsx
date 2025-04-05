b3e2c8341ede78bfc7a0cdcbaff5d0d9c6e7c921
MirjanaPanic <panicmirjanaa@gmail.com>
2025-04-05 19:49:14 +0200

    props

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,17 +1,25 @@
 //CHILD component - rendering inside its parent component
-export function Profile() {
-  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
+/*export function Profile({ opis, name = "Milos" }) {
+  console.log({ opis }, { name });
+  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt={opis} />;
+}
+*/
+export function Profile(props) {
+  console.log(props);
+  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt={props.name} />;
 }
 
 //PARENT component
 export default function Gallery() {
+  const alts = ["profil1", "profil2", "profil3", "profil4"];
+
   return (
     <section>
       <h1> Pictures: </h1>
-      <Profile />
-      <Profile />
-      <Profile />
-      <Profile />
+      <Profile opis={alts[0]} name="Mirjana" />
+      <Profile opis={alts[1]} />
+      <Profile opis={alts[2]} />
+      <Profile opis={alts[3]} />
     </section>
   );
 }
