d85a7a8470a11804ae065faf594b4bb78d2bdd99
MirjanaPanic <panicmirjanaa@gmail.com>
2025-04-07 11:55:55 +0200

    rendering lists - filter

--- src/components/Liste.jsx
+++ src/components/Liste.jsx
@@ -1,12 +1,17 @@
 const people = [
-  "Creola Katherine Johnson: mathematician",
-  "Mario José Molina-Pasquel Henríquez: chemist",
-  "Mohammad Abdus Salam: physicist",
-  "Percy Lavon Julian: chemist",
-  "Subrahmanyan Chandrasekhar: astrophysicist",
+  { id: 1, name: "Creola Katherine Johnson: mathematician", prof: "mat" },
+  { id: 2, name: "Mario José Molina-Pasquel Henríquez: chemist", prof: "che" },
+  { id: 3, name: "Mohammad Abdus Salam: physicist", prof: "phy" },
+  { id: 4, name: "Percy Lavon Julian: chemist", prof: "che" },
+  { id: 5, name: "Subrahmanyan Chandrasekhar: astrophysicist", prof: "ast" },
 ];
 
 export default function List() {
-  const listItems = people.map((person) => <li>{person}</li>);
+  const chemists = people.filter((person) => person.prof === "che");
+
+  const listItems = chemists.map((person) => (
+    <li key={person.id}>{person.name}</li>
+  ));
+
   return <ul>{listItems}</ul>;
 }
